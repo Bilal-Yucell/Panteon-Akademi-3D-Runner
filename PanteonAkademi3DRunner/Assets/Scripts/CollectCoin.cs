96298b04b2b6a4503dd00071e5fb8da44e69670b
Bilal-Yucell <bilallyucell@gmail.com>
2023-05-23 15:24:55 +0300

    Obstacle, End, Collision
    Obstacle, End, Collision

--- PanteonAkademi3DRunner/Assets/Scripts/CollectCoin.cs
+++ PanteonAkademi3DRunner/Assets/Scripts/CollectCoin.cs
@@ -1,11 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CollectCoin : MonoBehaviour
 {
+    public int score;
+    public TextMeshProUGUI coinText;
+    public PlayerController playerController;
+
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Coin"))
+        {
+            AddCoin();
+            Destroy(other.gameObject);
+        }
+        else if (other.CompareTag("End"))
+        {
+            Debug.Log("Congrats!");
+            playerController.runningSpeed = 0;
+        }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Collision"))
+        {
+            Debug.Log("Touched Obstacle!");
+        }
+    }
+
+    public void AddCoin()
+    {
+        score++;
+        coinText.text = "Score: " + score.ToString();
+    }
+
 }
